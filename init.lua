c6b9cc6127ca98cf0eee371a9e9a86b8ef8f62a8
dineshkumar <dineshkumar@arista.com>
2024-04-20 01:44:02 -0700

    Initial changes
    1. Enable clangd and pyright language servers
    1. Enable custom plugins
    2. Add plugins nvim-autopairs and nvim-neo-tree

--- init.lua
+++ init.lua
@@ -554,9 +554,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        -- pyright requires node; use brew install node
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -868,7 +869,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
